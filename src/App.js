f7feb88de2ba3d5b330dcf334b2358840ae99b17
ummfieg <ummfieg@naver.com>
2025-10-31 15:45:06 +0900

    feat(app): 구매자 구입 금액 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+import Buyer from "./Buyer.js";
+import Calculator from "./Calculator.js";
+
 class App {
-  async run() {}
+  async run() {
+    const purchaseAmount = await Console.readLineAsync(
+      "구매금액을 입력해 주세요\n"
+    );
+    Console.print(purchaseAmount);
+    const chooseNumbers = await Console.readLineAsync(
+      "당첨번호를 입력해 주세요\n"
+    );
+    const bonusNumber = await Console.readLineAsync(
+      "보너스 번호를 입력해 주세요\n"
+    );
+  }
 }
 
 export default App;
