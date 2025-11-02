f2cb8461bafa92c1547de8743421764c5775919c
ummfieg <ummfieg@naver.com>
2025-11-02 12:49:36 +0900

    refactor(validation): 숫자 검증 유틸 함수 - 배열, 문자열 분기처리로 수정

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,12 +1,24 @@
-//모두 (숫자, 공백)
-const ERROR_TEXT = "[ERROR]";
-export const validateBlank = (price) => {
-  if (!price || price.trim() === "") {
+export const ERROR_TEXT = "[ERROR]";
+//TODO: 에러메시지 재사용 로직으로 변경
+export const validateBlank = (value) => {
+  if (typeof value === "string" && value.trim() === "") {
     throw new Error(`${ERROR_TEXT} 금액을 입력해주세요.`);
   }
+  return value;
 };
-export const validateNumber = (price) => {
-  if (isNaN(Number(price))) {
-    throw new Error(` ${ERROR_TEXT} 숫자만 입력할 수 있습니다.`);
+export const validateNumber = (value) => {
+  if (Array.isArray(value)) {
+    return value.map((num) => {
+      const CONVERTED_NUMBERS = Number(num);
+      if (isNaN(CONVERTED_NUMBERS)) {
+        throw new Error(`${ERROR_TEXT} 숫자만 입력할 수 있습니다.`);
+      }
+      return CONVERTED_NUMBERS;
+    });
   }
+
+  const CONVERTED_NUMBERS = Number(value); //price로직, 보너스
+  if (isNaN(CONVERTED_NUMBERS))
+    throw new Error(`${ERROR_TEXT} 숫자만 입력할 수 있습니다.`);
+  return CONVERTED_NUMBERS;
 };
