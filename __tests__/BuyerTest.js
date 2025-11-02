306af32fb4c5476f4bb853d390423c3bcb24863e
ummfieg <ummfieg@naver.com>
2025-11-02 21:19:03 +0900

    refactor(buyer, buyerTest): 금액 필드 접근 제한자 및 테스트 로직 수정

--- __tests__/BuyerTest.js
+++ __tests__/BuyerTest.js
@@ -1,48 +1,75 @@
 import Buyer from "../src/Buyer";
 
-describe("구매자 클래스 금액 테스트", () => {
-  test("총 구입금액은 1,000원 단위가 아니면 예외가 발생한다.", () => {
-    //given
-    const INVALID_COST = "500";
-    // when-then
-    expect(() => {
-      new Buyer(INVALID_COST);
-    }).toThrow("[ERROR]");
-  });
+describe("구매자 클래스 테스트", () => {
+  describe("금액 입력값 검증 테스트", () => {
+    test("총 구입금액은 1,000원 단위가 아니면 예외가 발생한다.", () => {
+      //given
+      const INVALID_COST = "500";
+      // when-then
+      expect(() => {
+        new Buyer(INVALID_COST);
+      }).toThrow("[ERROR]");
+    });
 
-  test("총 구입금액은 1,000원 단위일 경우 통과", () => {
-    //givne
-    const VALID_COST = "5000";
-    // when
-    const VALIDATED_COST = new Buyer(VALID_COST);
-    //then
-    expect(VALIDATED_COST.price).toBe(5000);
-  });
-  test("금액 입력시 숫자가 아닐경우 예외가 발생한다.", () => {
-    //given
-    const INVALID_COST = ["stirng", "@"];
-    //when-then
-    INVALID_COST.forEach((cost) => {
-      expect(() => new Buyer(cost)).toThrow("[ERROR]");
+    test("총 구입금액은 1,000원 단위일 경우 통과", () => {
+      //givne
+      const VALID_COST = "5000";
+      // when
+      const RESULT = new Buyer(VALID_COST);
+      const VALIDATED_COST = RESULT.getPrice();
+      //then
+      expect(VALIDATED_COST).toBe(5000);
+    });
+
+    test("금액 입력시 숫자가 아닐경우 예외가 발생한다.", () => {
+      //given
+      const INVALID_COST = ["stirng", "@"];
+      //when-then
+      INVALID_COST.forEach((cost) => {
+        expect(() => new Buyer(cost)).toThrow("[ERROR]");
+      });
+    });
+
+    test("금액 입력시 공백을 입력할 경우 예외가 발생한다.", () => {
+      //given
+      const BLANK_INPUTS = ["  ", "\n", "\t"];
+      //when-then
+      BLANK_INPUTS.forEach((blank) => {
+        expect(() => new Buyer(blank)).toThrow("[ERROR]");
+      });
     });
   });
-
-  test("금액 입력시 공백을 입력할 경우 예외가 발생한다.", () => {
-    //given
-    const BLANK_INPUTS = ["  ", "\n", "\t"];
-    //when-then
-    BLANK_INPUTS.forEach((blank) => {
-      expect(() => new Buyer(blank)).toThrow("[ERROR]");
+  describe("금액 입력값 반환 테스트", () => {
+    test("입력한 총 금액을 구매한 갯수로 나타낼 수 있다.", () => {
+      //given
+      const INPUT_PRICE = "7000";
+      const EXPECTED_TICKET_COUNT = 7;
+      //when
+      const PRICE_CALC = new Buyer(INPUT_PRICE);
+      const TICKET_COUNT = PRICE_CALC.countTickets();
+      // then
+      expect(TICKET_COUNT).toBe(EXPECTED_TICKET_COUNT);
     });
-  });
-  test("입력한 총 금액을 구매한 갯수로 나타낼 수 있다.", () => {
-    //given
-    const INPUT_PRICE = "7000";
-    const EXPECTED_TICKET_COUNT = 7;
-    //when
-    const PRICE_CALC = new Buyer(INPUT_PRICE);
-    const TICKET_COUNT = PRICE_CALC.priceCalc();
-    // then
-    expect(TICKET_COUNT).toEqual(EXPECTED_TICKET_COUNT);
+
+    test("총 금액이 6000원일 때 구입 갯수는 6을 반환한다.", () => {
+      //when
+      const TICKET_CLAC = new Buyer(6000);
+      //then
+      expect(TICKET_CLAC.countTickets()).toBe(6);
+    });
+
+    test("로또 구입 갯수만큼 로또가 발행되어야 한다.", () => {
+      //given
+      const BUYER = new Buyer(6000);
+      //when
+      const TICKET_COUNT = BUYER.countTickets();
+      const RESULT = BUYER.randomNumbers();
+      //then
+      expect(RESULT.length).toBe(TICKET_COUNT);
+      // 각 티켓이 6개 숫자로 구성되었는지
+      RESULT.forEach((ticket) => {
+        expect(ticket.length).toBe(6);
+      });
+    });
   });
 });
--- src/Buyer.js
+++ src/Buyer.js
@@ -1,26 +1,43 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 import { validateBlank, validateNumber } from "./utils/validation.js";
 
 // 구매자가 구매한 갯수, 내역 관리
 class Buyer {
   static ERROR_TEXT = "[ERROR]";
+  #price;
+
   constructor(price) {
     validateBlank(price);
-    this.price = validateNumber(price);
-    this.#validatePirce();
+    const VALID_PRICE = validateNumber(price);
+    this.#validatePirce(VALID_PRICE);
+    this.#price = VALID_PRICE;
+    this.getPrice();
   }
-  #validatePirce() {
-    if (this.price % 1000 !== 0) {
+  #validatePirce(VALID_PRICE) {
+    if (VALID_PRICE % 1000 !== 0) {
       throw new Error(
         ` ${Buyer.ERROR_TEXT} 입력할 수 있는 금액은 1000원 단위여야 합니다.`
       );
     }
   }
 
-  priceCalc() {
-    const TICKET_COUNT = this.price / 1000;
-    Console.print(`${TICKET_COUNT}개를 구매했습니다.`);
+  countTickets() {
+    const TICKET_COUNT = this.#price / 1000;
     return TICKET_COUNT;
+  }
+
+  randomNumbers() {
+    const ROUND_COUNT = this.countTickets();
+    const ROUND_NUMBERS = Object.freeze(
+      Array.from({ length: ROUND_COUNT }, () =>
+        Object.freeze(Random.pickUniqueNumbersInRange(1, 45, 6))
+      )
+    );
+    return ROUND_NUMBERS;
+  }
+
+  getPrice() {
+    return this.#price;
   }
 }
 
