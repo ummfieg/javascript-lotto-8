53c89542fa34f52b02a28700f443e07b36cfc1fb
ummfieg <ummfieg@naver.com>
2025-11-03 15:36:50 +0900

    refactor(calculatorTest): 당첨 내역 테스트 케이스 추가

--- __tests__/CalculatorTest.js
+++ __tests__/CalculatorTest.js
@@ -11,22 +11,81 @@ describe("계산 클래스 테스트", () => {
     bonusNum = [7];
     inputPrice = 1000;
   });
+  describe("당첨 금액 반환 테스트", () => {
+    test("당첨번호가 3개 일치 시 5,000원을 반환한다.", () => {
+      //given
+      const WIN_NUM = [1, 2, 3, 10, 11, 12];
+      const MATCH_LIST = new Calculator(
+        publishedNum,
+        WIN_NUM,
+        bonusNum,
+        inputPrice
+      );
+      //when
+      const MATCH_RESULT = MATCH_LIST.calculateTotalPrize();
+      //then
+      expect(MATCH_RESULT).toBe(5000);
+    });
 
-  test("당첨번호와 발행번호가 일치 한 총 상금을 계산한다.", () => {
-    //given
-    const WIN_NUM = [1, 2, 3, 4, 35, 20];
-    const MATCH_LIST = new Calculator(
-      publishedNum,
-      WIN_NUM,
-      bonusNum,
-      inputPrice
-    );
-    //when
-    const MATCH_RESULT = MATCH_LIST.calculateTotalPrize();
-    //then
-    expect(MATCH_RESULT).toBe(50000);
+    test("당첨번호가 4개 일치 시 50,000원을 반환한다.", () => {
+      //given
+      const WIN_NUM = [1, 2, 3, 4, 35, 20];
+      const MATCH_LIST = new Calculator(
+        publishedNum,
+        WIN_NUM,
+        bonusNum,
+        inputPrice
+      );
+      //when
+      const MATCH_RESULT = MATCH_LIST.calculateTotalPrize();
+      //then
+      expect(MATCH_RESULT).toBe(50000);
+    });
+    test("당첨번호가 5개 일치 시 1,500,000원을 반환한다.", () => {
+      //given
+      const WIN_NUM = [1, 2, 3, 4, 5, 10];
+      const MATCH_LIST = new Calculator(
+        publishedNum,
+        WIN_NUM,
+        bonusNum,
+        inputPrice
+      );
+      //when
+      const MATCH_RESULT = MATCH_LIST.calculateTotalPrize();
+      //then
+      expect(MATCH_RESULT).toBe(1500000);
+    });
+    test("당첨번호 5개 + 보너스 일치 시30,000,000원을 반환한다.", () => {
+      //given
+      const WIN_NUM = [1, 2, 3, 4, 5, 10];
+      const BONUS_NUM = [6];
+      const MATCH_LIST = new Calculator(
+        publishedNum,
+        WIN_NUM,
+        BONUS_NUM,
+        inputPrice
+      );
+      //when
+      const MATCH_RESULT = MATCH_LIST.calculateTotalPrize();
+      //then
+      expect(MATCH_RESULT).toBe(30000000);
+    });
+    test("당첨번호 6개 일치 시 2,000,000,000원을 반환한다.", () => {
+      //given
+      const WIN_NUM = [1, 2, 3, 4, 5, 6];
+
+      const MATCH_LIST = new Calculator(
+        publishedNum,
+        WIN_NUM,
+        bonusNum,
+        inputPrice
+      );
+      //when
+      const MATCH_RESULT = MATCH_LIST.calculateTotalPrize();
+      //then
+      expect(MATCH_RESULT).toBe(2000000000);
+    });
   });
-
   test("당첨금액의 수익률을 계산한다.", () => {
     //given
     const WIN_NUM = [1, 2, 3, 7, 35, 20];
